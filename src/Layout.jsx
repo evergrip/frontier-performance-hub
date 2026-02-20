079b5c3077784eb3ea0f55a7ae887db3d98be4f5
christopher jagt <christopher.jagt@gmail.com>
2026-02-20 14:50:22 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,7 +4,7 @@ import { createPageUrl } from './utils';
 import { base44 } from '@/api/base44Client';
 import { 
   LayoutDashboard, Users, Target, Briefcase, Building2, 
-  Settings, Menu, X, ChevronRight, LogOut, DollarSign, CalendarDays, Upload, Flag, Wrench, MessageSquare 
+  Settings, Menu, X, ChevronRight, LogOut, DollarSign, CalendarDays, Upload, Flag, Wrench, MessageSquare, Megaphone 
 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
