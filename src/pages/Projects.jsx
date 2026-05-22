a3040458edda1de6ad5b0ba936bd7c90c498e9c1
christopher jagt <christopher.jagt@gmail.com>
2026-05-22 11:44:11 +0000

    File changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -102,7 +102,10 @@ export default function Projects() {
 
   const { data: users = [] } = useQuery({
     queryKey: ['users'],
-    queryFn: () => base44.entities.User.list(),
+    queryFn: async () => {
+      const response = await base44.functions.invoke('listUsersBasic');
+      return response.data?.users || [];
+    },
     initialData: [],
   });
 
