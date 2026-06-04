09a75ed4c346759aeaad3e6b034ece69002f3273
christopher jagt <christopher.jagt@gmail.com>
2026-06-04 19:37:59 +0000

    File changes

--- base44/functions/recalcSurveyScores/entry.ts
+++ base44/functions/recalcSurveyScores/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.23';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.31';
 
 function evaluateVisibility(question, responses) {
   const rules = question.logic_rules;
